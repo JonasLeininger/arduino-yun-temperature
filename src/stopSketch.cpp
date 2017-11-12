ece6720a228ae0f8967cd1bfa25e6803dd599aa9
Jonas Leininger <jonas.leininger@gmail.com>
2017-11-12 12:01:14 +0100

    little clean up

---
+++ src/stopSketch.cpp
@@ -0,0 +1,9 @@
+/*
+ * Stop script
+ */
+
+#include <Arduino.h>
+
+void setup(){}
+
+void loop(){}
--- sshupload.py
+++ sshupload.py
@@ -6,4 +6,8 @@ address = env.Dump("UPLOAD_PORT").replace('\'', '')
 def ssh_upload(target, source, env):
     print "Attempting to upload firmware via SSH to %s" % address
     firmware = str(target[0])
-    
+    os.system('scp '+firmware+' root@'+address+':/root/firmware.hex')
+    os.system('ssh root@'+address+' run-avrdude /root/firmware.hex')
+    exit()
+
+env.AddPreAction("upload", ssh_upload)
